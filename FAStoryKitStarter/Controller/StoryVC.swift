c23f3e403ec5b1e77848420dcd3a41641b40bc95
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2019-09-29 15:43:24 +0300

    Container controller modification is done

--- FAStoryKitStarter/Controller/StoryVC.swift
+++ FAStoryKitStarter/Controller/StoryVC.swift
@@ -47,6 +47,9 @@ class StoryVC: UIViewController, TransitionTransparencyProxy {
     /// ViewController to display the story content
     private var storyVc: FAStoryViewController!
     
+    /// Story container viewController
+    private weak var storyContainerVc: FAStoryContainer!
+    
     /// Story server
     private var storyHandler = StoryServer()
     // -----------------------------------
@@ -64,19 +67,7 @@ class StoryVC: UIViewController, TransitionTransparencyProxy {
         super.viewDidLoad()
         setupUI()
     }
-    
-    override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
-      
-        super.present(viewControllerToPresent, animated: flag, completion: completion)
-        
-        
-        transitionCoordinator?.animate(alongsideTransition: { (_) in
-            self.transparentTopView.isHidden = false
-            self.transparentTopView.backgroundColor = .black
-        })
-        
-    }
-    
+ 
     override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
         super.dismiss(animated: flag, completion: completion)
         
@@ -85,6 +76,7 @@ class StoryVC: UIViewController, TransitionTransparencyProxy {
         }, completion: { (_) in
             self.transparentTopView.isHidden = true
             self.storyVc = nil
+            self.storyContainerVc = nil 
         })
     }
     
@@ -150,6 +142,10 @@ class StoryVC: UIViewController, TransitionTransparencyProxy {
         guard let _stories = storyHandler.stories else {return}
         guard idx >= 0 && idx < _stories.count else {return}
         
+        /// Alternative to show one highligh at a time
+        /// without using the container
+        
+        /*
         storyVc = FAStoryViewController()
         storyVc.delegate = self
         storyVc.story = _stories[idx]
@@ -158,6 +154,20 @@ class StoryVC: UIViewController, TransitionTransparencyProxy {
         storyVc.modalPresentationCapturesStatusBarAppearance = true
         
         present(storyVc, animated: true)
+        */
+        
+        /// Alternative to show the container
+        storyVc = FAStoryViewController()
+        storyVc.delegate = self
+        storyVc.story = _stories[idx]
+        
+        storyContainerVc = FAStoryContainer(storyController: storyVc)
+        storyContainerVc.modalPresentationStyle = .overFullScreen
+        storyContainerVc.transitioningDelegate  = self
+        
+        
+        present(storyContainerVc, animated: true)
+    
     }
 
 
@@ -216,5 +226,52 @@ extension StoryVC: FAStoryDataSource {
 
 }
 
+
+
+// ==================================================== //
+// MARK: UIViewControllerTransitioningDelegate
+// ==================================================== //
+extension StoryVC: UIViewControllerTransitioningDelegate, ModalPresentationAnimatorProtocol {
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        let t = ModalPresentationAnimator(duration: 0.8, dismiss: false, topDistance: 0, rasterizing: false)
+        t.delegate = self
+        return t
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        let _transition = ModalPresentationAnimator(duration: 0.3, dismiss: true, topDistance: 0, rasterizing: false)
+        
+        _transition.delegate = self
+        
+        //
+        // Check the interaction controller
+        //
+        if let _interactor = (dismissed as? FAStoryViewController)?.dismissInteractionController {
+            _transition.dismissInteractionController = _interactor
+        } else if let _interactor = (dismissed as? FAStoryContainer)?.dismissInteractionController {
+            _transition.dismissInteractionController = _interactor
+        }
+        return _transition
+    }
+    
+    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        guard let animator = animator as? MainTransitionAnimator,
+            let interactionController = animator.dismissInteractionController,
+            interactionController.interactionInProgress
+            else {
+                return nil
+        }
+        return interactionController
+    }
+
+    
+    func didComplete(_ completed: Bool, isDismissal: Bool) {
+        if completed && isDismissal, storyVc != nil {
+            storyVc = nil
+        }
+    }
+    
+}
 
 
